c172b147d6eb19cb5bc1084762a22db307b82afd
Carsten König <Carsten.Koenig@hotmail.de>
2022-12-02 14:00:18 +0100

    2017 - day 12 - Star 1

--- Solutions/src/Y2017/Day12/Solution.hs
+++ Solutions/src/Y2017/Day12/Solution.hs
@@ -1,13 +1,25 @@
 -- Description : Advent of Code - 2015 / Tag 1
 --
 -- Siehe [Problestellung](https://adventofcode.com/2015/day/1)
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
 module Y2017.Day12.Solution where
 
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as Set
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as PC
+import Tools.CommonParsers (Parser)
+import qualified Tools.CommonParsers as CP
+
 yearNr :: Int
-yearNr = 2015
+yearNr = 2017
 
 dayNr :: Int
-dayNr = 1
+dayNr = 12
 
 -- | Lösung des Tages berechnen und ausgeben
 run :: IO ()
@@ -28,7 +40,10 @@ run = do
 -- solutions
 
 part1 :: Input -> Int
-part1 _ = 0
+part1 inp =
+  Set.size $ connectedGroupOf g 0
+ where
+  g = createGraph inp
 
 part2 :: Input -> Int
 part2 _ = 0
@@ -36,7 +51,47 @@ part2 _ = 0
 ----------------------------------------------------------------------
 -- data model
 
-type Input = ()
+type Input = [Definition]
+
+type PrgId = Int
+
+data Definition = Definition
+  { prgId :: PrgId
+  , canComWith :: IntSet
+  }
+  deriving (Show)
+
+type Definitions = IntMap Definition
+
+createDefinitions :: Input -> Definitions
+createDefinitions defs =
+  IntMap.fromList [(prgId d, d) | d <- defs]
+
+type Graph = IntMap IntSet
+
+createGraph :: Input -> Graph
+createGraph defs =
+  IntMap.fromList [(prgId d, canComWith d) | d <- defs]
+
+connectedGroupOf :: Graph -> PrgId -> IntSet
+connectedGroupOf graph pId =
+  go Set.empty (Set.singleton pId)
+ where
+  go visited lookAt =
+    case Set.minView lookAt of
+      Nothing -> visited
+      Just (prgId', rest) ->
+        let visited' = Set.insert prgId' visited
+            reachable = graph IntMap.! prgId'
+            shouldBeVisted = Set.difference reachable visited'
+         in go visited' (Set.union rest shouldBeVisted)
+
+connectGroups :: Graph -> [IntSet]
+connectGroups graph = go allNodes
+ where
+  go :: IntSet -> [IntSet]
+  go = undefined
+  allNodes = Set.fromList (IntMap.keys graph)
 
 ----------------------------------------------------------------------
 -- load and parse input
@@ -50,6 +105,32 @@ loadFile file = do
   pure $ parseText txt
 
 parseText :: String -> Input
-parseText txt =
-  let _ = lines txt
-   in ()
+parseText = map (CP.runParser definitionP) . lines
+
+-- >>> CP.runParser prgIdP "3"
+-- 3
+prgIdP :: Parser PrgId
+prgIdP = CP.numberP
+
+-- >>> CP.runParser canComWithP "0, 3, 4"
+-- fromList [0,3,4]
+canComWithP :: Parser IntSet
+canComWithP = do
+  prgs <- prgIdP `P.sepBy1` PC.string ", "
+  return $ Set.fromList prgs
+
+-- >>> CP.runParser definitionP "2 <-> 0, 3, 4"
+-- Definition {prgId = 2, canComWith = fromList [0,3,4]}
+definitionP :: Parser Definition
+definitionP = do
+  pId <- prgIdP
+  _ <- PC.string " <-> "
+  canCom <- canComWithP
+  pure $ Definition pId canCom
+
+-- >>> test
+-- [(1,2),(1,3),(1,4),(2,2),(2,3),(2,4),(3,2),(3,3),(3,4),(4,2),(4,3),(4,4),(5,2),(5,3),(5,4)]
+test = do
+  x <- [1 .. 5]
+  y <- [2 .. 4]
+  pure (x, y)
