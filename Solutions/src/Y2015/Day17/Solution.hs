5fb66c892b208bdd23361fcdafa8ddfce9d71bc4
Carsten König <Carsten.Koenig@hotmail.de>
2022-12-02 10:56:45 +0100

    2015 - 17

--- Solutions/src/Y2015/Day17/Solution.hs
+++ Solutions/src/Y2015/Day17/Solution.hs
@@ -3,6 +3,8 @@
 -- Siehe [Problestellung](https://adventofcode.com/2015/day/17)
 module Y2015.Day17.Solution where
 
+import Data.Function.Memoize (memoFix2)
+
 yearNr :: Int
 yearNr = 2015
 
@@ -31,7 +33,7 @@ part1 :: Input -> Int
 part1 = length . combsFor 150
 
 part2 :: Input -> Int
-part2 _ = 0
+part2 = minCombsFor 150
 
 ----------------------------------------------------------------------
 -- data model
@@ -40,6 +42,13 @@ type Input = [Container]
 
 type Container = Int
 type Volume = Int
+
+minCombsFor :: Volume -> [Container] -> Int
+minCombsFor vol conts =
+  length $ filter (\cs -> length cs == minCount) allCombs
+ where
+  allCombs = combsFor vol conts
+  minCount = minimum $ map length allCombs
 
 -- >>> length (combsFor 25 [20,15,10,5,5]) == 4
 -- True
@@ -51,6 +60,17 @@ combsFor vol (cont : more)
   | otherwise =
       [cont : comb | comb <- combsFor (vol - cont) more]
         ++ combsFor vol more
+
+combsFor' :: Volume -> [Container] -> [[Container]]
+combsFor' = memoFix2 go
+ where
+  go _ 0 _ = [[]]
+  go _ _ [] = []
+  go f vol (cont : more)
+    | cont > vol = f vol more
+    | otherwise =
+        [cont : comb | comb <- f (vol - cont) more]
+          ++ f vol more
 
 ----------------------------------------------------------------------
 -- load and parse input
